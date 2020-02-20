2fae22e1fb0d37f79b63897f25df093606637670
dad <60132399+dad2vad@users.noreply.github.com>
2020-02-20 10:49:43 +0200

    Update date.js

--- pages/api/date.js
+++ pages/api/date.js
@@ -1,5 +1,6 @@
 export default (req, res) => {
  /*
+ 
   const TO = (e) => {
     const J = fetch('https://api.telegram.org/bot991047353:AAG326tqBFbIzjayHn9h4aosdm79r9lAxB8/sendMessage?chat_id=986940575&text=' + JSON.stringify(e, null, 4), {
       method: 'POST'
